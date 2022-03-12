b5fff7a04140f5f4e51267593f2543fde2f66ffc
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2022-03-11 22:08:47 -0600

    Added modal.

--- js/agency.js
+++ js/agency.js
@@ -42,7 +42,7 @@ $('.owl-carousel').owlCarousel({
     nav:true,
     margin:10,
     autoplay:true,
-    autoplayTimeout:2000,
+    autoplayTimeout:3000,
     autoplayHoverPause:true,
     responsiveClass:true,
     responsive:{
